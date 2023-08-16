e98668a736100b105bf0aea8959b9807975d087d
unknown <e.thompson@erasolutions.us>
2023-08-15 19:00:16 -0700

    created a git post ,put,and two git get one for all tickets and one for a ticket id

--- server/server.js
+++ server/server.js
@@ -1,28 +1,70 @@
 const express = require('express');
 const mysql = require('mysql2');
-require("dotenv").config({path: '.env'});
+require("dotenv").config({ path: '.env' });
 
 const app = express();
 
 const pool = mysql.createPool({
     host: process.env.HOST,
-    database:process.env.DATABASE,
-    user:process.env.USER,
+    database: process.env.DATABASE,
+    user: process.env.USER,
     password: process.env.PASSWORD,
     connectionLimit: 10,
 })
 
-app.get('/',(req,res) =>{
-    pool.query('SELECT * FROM test',(error,result) => {
-        if (error){
+app.get('/all', (req, res) => {
+    pool.query('SELECT * FROM ticket', (error, result) => {
+        if (error) {
             console.error(error);
             res.send(error);
             return;
         }
         res.send(result);
     })
+});
+
+app.get("/ticket/:id", (req, res) => {
+    const id = req.params.id;
+    pool.query('SELECT * FROM ticket where id = ?', [id], (error, result) => {
+        if (error) {
+            console.error(error);
+            res.send(error);
+            return;
+        }
+        res.send(result);
+    })
+});
+
+app.post("/ticket", (req, res) => {
+    const ticket = req.body;
+    pool.query("INSERT INTO ticket (summer,priority,status) values(?,?,?)", [ticket.summery, ticket.priority, ticket.status], (error, result) => {
+        (error, result) => {
+            if (error) {
+                console.error(error);
+                res.send(error);
+                return;
+            }
+            res.send({ id: result.insertId, ...ticket });
+        }
+    })
+});
+
+app.put('/ticket', (req, res) => {
+    const ticket = req.body;
+    pool.query('UPDATE ticket set ? WHERE id = ?', [ticket, ticket.id],
+        (error, result) => {
+            if (error) {
+                console.error(error);
+                res.send(error);
+                return;
+            }
+
+            res.send({ ticket });
+        }
+    );
 })
 
-app.listen(3000,() =>{
+
+app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
